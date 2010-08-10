5c55aff1b175232002de297c67380a403c810ca4
John Kenyon <JLKenyon@gmail.com>
2010-08-11 00:34:32 +0200

    Adding stubs for start to 2.3.2 and modified makefile to force building

--- scheme/src/Sicp/Ch2/Sec3/Part2.scm
+++ scheme/src/Sicp/Ch2/Sec3/Part2.scm
@@ -7,3 +7,15 @@
     (display "sicp-main-ch2-sec3-part2\n")
 )
 
+(define (variable? e) #f)
+(define (same-variable? v1 v2) #f)
+
+(define (sum? e) #f)
+(define (addend e) #f) 
+(define (augend e) #f) 
+(define (make-sum a1 a2) #f)
+
+(define (product? e) #f)
+(define (multiplier e) #f)
+(define (multiplicand e) #f)
+(define (make-product m1 m2) #f)
