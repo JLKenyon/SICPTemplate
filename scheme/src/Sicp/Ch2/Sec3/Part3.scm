76e03bbbec7580a53462b61eccd771d416c8c19c
John Kenyon <JLKenyon@gmail.com>
2010-08-15 10:56:41 +0200

    added stubs for 2.3.3

--- scheme/src/Sicp/Ch2/Sec3/Part3.scm
+++ scheme/src/Sicp/Ch2/Sec3/Part3.scm
@@ -7,3 +7,17 @@
     (display "sicp-main-ch2-sec3-part3\n")
 )
 
+(define (create-set values) #f)
+(define (element-of-set? x set) #f)
+(define (adjoin-set x set) #f)
+(define (intersection-set set1 set2) #f)
+
+; 2.59
+(define (union-set set1 set2) #f)
+
+
+; 2.60
+(define (create-repset values) #f)
+(define (element-of-repset? x set) #f)
+(define (adjoin-repset x set) #f)
+(define (intersection-repset set1 set2) #f)
