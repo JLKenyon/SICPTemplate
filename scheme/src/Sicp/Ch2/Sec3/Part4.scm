e4a1891c93468bc2d36c3d408ff4bc526d71dc76
John Kenyon <JLKenyon@gmail.com>
2010-08-15 12:32:11 +0200

    added section 2.3.4 stubs

--- scheme/src/Sicp/Ch2/Sec3/Part4.scm
+++ scheme/src/Sicp/Ch2/Sec3/Part4.scm
@@ -7,3 +7,35 @@
     (display "sicp-main-ch2-sec3-part4\n")
 )
 
+; Representing Huffman trees
+
+(define (huftree:make-leaf      symbol weight) #f)
+(define (huftree:leaf?          object)        #f)
+(define (huftree:symbol-leaf    x)             #f)
+(define (huftree:weight-leaf    x)             #f)
+
+(define (huftree:make-code-tree left right)    #f)
+(define (huftree:left-branch    tree)          #f)
+(define (huftree:right-branch   tree)          #f)
+(define (huftree:symbols        tree)          #f)
+(define (huftree:weight         tree)          #f)
+
+; The decoding procedure
+(define (huftree:decode         bits tree) #f)
+(define (huftree:choose-branch  bit branch) #f)
+
+; Sets of weighted elements
+(define (huftree:adjoin-set     x set) #f)
+(define (huftree:make-leaf-set  pairs) #f)
+
+; 2.67
+(define (huftree:sample-tree) #f)
+(define huftree:sample-message '())
+
+; 2.68
+(define (encode message tree) #f)
+
+; 2.69
+(define (huftree:generate-huffman-tree pairs) #f)
+
+; 2.70 - 2.72 - no function pre-defined
